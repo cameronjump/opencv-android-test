54708e38d5ea80a0329900315ff520e5375a0b96
cameronjump <cameron.jump@okstate.edu>
2018-12-17 13:11:12 -0600

    video and lodas classifier

--- AndroidTest/app/src/main/java/cameronjump/test/FaceActivity.kt
+++ AndroidTest/app/src/main/java/cameronjump/test/FaceActivity.kt
@@ -1,6 +1,7 @@
 package cameronjump.test
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Matrix
@@ -8,23 +9,32 @@ import android.media.ExifInterface
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.SurfaceView
+import cameronjump.test.R.xml.haarcascade_frontalface_alt
 import kotlinx.android.synthetic.main.activity_face.*
 import org.opencv.android.CameraBridgeViewBase
 import org.opencv.android.OpenCVLoader
 import org.opencv.core.Core
 import org.opencv.core.Core.rotate
 import org.opencv.core.Mat
+import org.opencv.core.MatOfRect
 import org.opencv.imgproc.Imgproc
+import org.opencv.objdetect.CascadeClassifier
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 class FaceActivity: AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListener2 {
 
     private val TAG = "FaceActivityDebug"
     private val REQUEST_IMAGE_CAPTURE = 200
-    private lateinit var uri: Uri
     private lateinit var mOpenCvCameraView: CameraBridgeViewBase
+    private lateinit var mCascadeClassifier: CascadeClassifier
+    //private val mClassifierName = haarcascade_frontalface_alt
     private val init = initialization()
 
     class initialization {
@@ -44,15 +54,13 @@ class FaceActivity: AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListen
         mOpenCvCameraView.setCvCameraViewListener(this)
         mOpenCvCameraView.setCameraIndex(1)
 
-        face_button.setOnClickListener( {
-            startCamera()
-        })
-    }
+        val cascadeDir = getDir("xml", Context.MODE_PRIVATE)
+        var mCascadeFile = File(cascadeDir,"haarcascade_frontalface_alt")
+        mCascadeClassifier = CascadeClassifier(mCascadeFile.absolutePath)
 
-    private fun startCamera() {
-        //uri = FileProvider.getUriForFile(this, "cameronjump.test.fileprovider",
-        //        File.createTempFile("picture", ".jpg", getExternalFilesDir(Environment.DIRECTORY_PICTURES)))
-        //startActivityForResult(Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply { putExtra(MediaStore.EXTRA_OUTPUT, uri) }, REQUEST_IMAGE_CAPTURE)
+
+        loadClassifier()
+
     }
 
     public override fun onPause() {
@@ -84,6 +92,10 @@ class FaceActivity: AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListen
         Core.flip(mRgbaT, mRgbaT, -1)
         Imgproc.resize(mRgbaT, mRgbaT, mRgba.size())
 
+        var faces = MatOfRect()
+
+        mCascadeClassifier.detectMultiScale(mRgbaT, faces)
+        
         return mRgbaT
     }
 
@@ -97,13 +109,35 @@ class FaceActivity: AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListen
         }
     }
 
-    private fun getCapturedImage(uri: Uri): Bitmap {
-        val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
-        return when (ExifInterface(contentResolver.openInputStream(uri)).getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)) {
-            ExifInterface.ORIENTATION_ROTATE_90 -> Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, Matrix().apply { postRotate(90F) }, true)
-            ExifInterface.ORIENTATION_ROTATE_180 -> Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, Matrix().apply { postRotate(180F) }, true)
-            ExifInterface.ORIENTATION_ROTATE_270 -> Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, Matrix().apply { postRotate(270F) }, true)
-            else -> bitmap
+    fun loadClassifier() {
+        try {
+            // load cascade file from application resources
+            val inputStream = resources.openRawResource(R.raw.haarcascade_frontalface_alt)
+            val cascadeDir = getDir("cascade", Context.MODE_PRIVATE)
+            val mCascadeFile = File(cascadeDir, "haarcascade_frontalface_alt.xml")
+            val outputStream = FileOutputStream(mCascadeFile)
+
+            val buffer = ByteArray(4096)
+            var bytesRead: Int = inputStream.read(buffer)
+            while (bytesRead != -1) {
+                outputStream.write(buffer, 0, bytesRead)
+                bytesRead = inputStream.read(buffer)
+            }
+            inputStream.close()
+            outputStream.close()
+
+            mCascadeClassifier = CascadeClassifier(mCascadeFile.absolutePath)
+            if (mCascadeClassifier.empty()) {
+                Log.d(TAG, "Failed to load cascade classifier")
+            } else
+                Log.d(TAG, "Loaded cascade classifier from " + mCascadeFile.absolutePath)
+
+
+            cascadeDir.delete()
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Log.d(TAG, "Failed to load cascade. Exception thrown: $e")
         }
     }
 }
--- AndroidTest/openCVLibrary343/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ AndroidTest/openCVLibrary343/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/cameronjump/Projects/AndroidTest/openCVLibrary343/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /Users/cameronjump/Projects/opencv-android-test/AndroidTest/openCVLibrary343/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
